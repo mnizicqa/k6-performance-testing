e6dcfaa2638bd3d7a9db1eeeb9d0a4d4eeed47c3
Mario Nizic <marionizic@msn.com>
2024-12-13 22:19:39 +0100

    Added script for groups in k6

---
+++ groups.js
@@ -0,0 +1,32 @@
+import http from "k6/http";
+import { sleep, check, group } from "k6";
+
+export const options = {
+  thresholds: {
+    http_req_duration: ["p(95)<300"],
+    "group_duration{group:::Main page}": ["p(95)<250"],
+    "group_duration{group:::Main page::Assets}": ["p(95)<250"],
+  },
+};
+
+export default () => {
+  group("Main page", () => {
+    let res = http.get("https://test.k6.io");
+    check(res, {
+      "status is 200": (r) => res.status === 200,
+    });
+    group("Assets", () => {
+      http.get("https://test.k6.io/static/css/site.css");
+      http.get("https://test.k6.io/static/js/prisms.js");
+    });
+  });
+
+  group("Contacts page", () => {
+    http.get("https://test.k6.io/contacts.php");
+  });
+
+  group("News page", () => {
+    http.get("https://test.k6.io/news.php");
+  });
+  sleep(1);
+};
