1ca3c81cf203977a4a7ea6fa39c17c51203ba894
Mario Nizic <marionizic@msn.com>
2024-12-15 12:37:40 +0100

    Added script for test abortion in k6

---
+++ abort-test.js
@@ -0,0 +1,20 @@
+import http from "k6/http";
+import { sleep } from "k6";
+import exec from "k6/execution";
+
+export const options = {
+  vus: 10,
+  duration: "60s",
+};
+
+export function setup() {
+  let res = http.get("https://test.k6.local/status");
+  if (res.error) {
+    exec.test.abort("Aborting test. Application is down!");
+  }
+}
+
+export default function () {
+  http.get("https://test.k6.local/some-page");
+  sleep(1);
+}
