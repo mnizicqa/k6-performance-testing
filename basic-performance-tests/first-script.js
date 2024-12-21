009927b46722d821a9b0a76e6e2daa0f335896ad
Mario Nizic <marionizic@msn.com>
2024-12-21 16:17:16 +0100

    Added script for k6 testing using k6 cloud, updated first-script.js for testing on k6 cloud

--- basic-performance-tests/first-script.js
+++ basic-performance-tests/first-script.js
@@ -2,8 +2,11 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export const options = {
-  vus: 10,
+  vus:1,
   duration: "10s",
+  cloud: {
+    projectID: 3731474
+  }
 };
 
 export default function () {
